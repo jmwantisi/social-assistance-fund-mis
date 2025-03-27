4f0766079f401dd8a1e2fd02bfc95e9b9cbb57bc
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-03-27 02:26:18 +0200

    add inital program startup dependencies

--- socialAssistanceFundMIS/Program.cs
+++ socialAssistanceFundMIS/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using socialAssistanceFundMIS.Components;
+using socialAssistanceFundMIS.Data;
+using socialAssistanceFundMIS.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +9,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Register and Inject DB Context
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Run database migrations and seed data on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate(); // Apply pending migrations
+    GeographicLocationSeeder.SeedGeographicLocations(context);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
