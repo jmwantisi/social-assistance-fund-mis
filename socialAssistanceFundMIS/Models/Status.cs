fa4bfde87fe8850d83a6f4a69bca651b5e1e995a
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-03-27 02:34:35 +0200

    add model: status

---
+++ socialAssistanceFundMIS/Models/Status.cs
@@ -0,0 +1,15 @@
+﻿namespace socialAssistanceFundMIS.Models
+{
+    public class Status
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Removed { get; set; } = 0;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
