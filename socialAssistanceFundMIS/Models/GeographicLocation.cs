7c3125e1a1ceaaa39b05486f88d3735fbc4b398a
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-03-27 02:33:21 +0200

    add model: geographical location

---
+++ socialAssistanceFundMIS/Models/GeographicLocation.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace socialAssistanceFundMIS.Models
+{
+    public class GeographicLocation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public int GeographicLocationTypeId { get; set; }
+        public GeographicLocationType? GeographicLocationType { get; set; }
+
+        [Required]
+        public int GeographicLocationParentId { get; set; }
+        public GeographicLocation? ParentLocation { get; set; }
+
+        public int Removed { get; set; } = 0;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
