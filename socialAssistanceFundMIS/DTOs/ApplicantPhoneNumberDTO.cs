850f5381ebec86b0746f6c9cd09d53b2dcb31c16
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-03-27 05:39:02 +0200

    refactor

--- socialAssistanceFundMIS/DTOs/ApplicantPhoneNumberDTO.cs
+++ socialAssistanceFundMIS/DTOs/ApplicantPhoneNumberDTO.cs
@@ -1,7 +1,9 @@
 ﻿public class ApplicantPhoneNumberDTO
 {
     public int Id { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
     public int PhoneNumberTypeId { get; set; }
-    public string PhoneNumberType { get; set; }  // Add human-readable phone number type
+    public string? PhoneNumberType { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
 }
