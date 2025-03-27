6a92eede12a9aced86b226bf5db426f565fe2c8f
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-03-27 02:32:41 +0200

    add model: assistance program

---
+++ socialAssistanceFundMIS/Models/AssistanceProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace socialAssistanceFundMIS.Models
+{
+    public class AssistanceProgram
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Removed { get; set; } = 0;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+
+}
