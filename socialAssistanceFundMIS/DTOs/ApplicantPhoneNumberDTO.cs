489c42fce59b5130eac99d4867731a9209e152b2
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-03-27 04:54:57 +0200

    add DTO: applicant phone number

---
+++ socialAssistanceFundMIS/DTOs/ApplicantPhoneNumberDTO.cs
@@ -0,0 +1,7 @@
+﻿public class ApplicantPhoneNumberDTO
+{
+    public int Id { get; set; }
+    public string PhoneNumber { get; set; }
+    public int PhoneNumberTypeId { get; set; }
+    public string PhoneNumberType { get; set; }  // Add human-readable phone number type
+}
