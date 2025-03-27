be2d793eedb792b614a5aad4053691841b843279
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-03-27 02:36:05 +0200

    add registration service

---
+++ socialAssistanceFundMIS/Services/RegistrationService.cs
@@ -0,0 +1,27 @@
+﻿using socialAssistanceFundMIS.Data;
+using socialAssistanceFundMIS.Models;
+
+namespace socialAssistanceFundMIS.Services
+{
+    public interface IRegistrationService
+    {
+        Task<bool> SubmitApplication(Application application);
+    }
+
+    public class RegistrationService : IRegistrationService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RegistrationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SubmitApplication(Application application)
+        {
+            _context.Applications.Add(application);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+
+}
