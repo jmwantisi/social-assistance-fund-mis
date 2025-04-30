876845bffd65f89544aa0c5d39b5afdc703ee9b0
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-30 12:28:50 +0200

    refactor program

--- socialAssistanceFundMIS/Program.cs
+++ socialAssistanceFundMIS/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using socialAssistanceFundMIS.Components;
 using socialAssistanceFundMIS.Data;
 using socialAssistanceFundMIS.Seeders;
+using socialAssistanceFundMIS.Services;
+using SocialAssistanceFundMisMcv.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,24 @@ builder.Services.AddRazorComponents()
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerExpressEdition")));
+
+// Services
+
+builder.Services.AddScoped<ApplicantService>();
+builder.Services.AddScoped<ApplicationService>();
+builder.Services.AddScoped<AssistanceProgramService>();
+builder.Services.AddScoped<DesignationService>();
+builder.Services.AddScoped<GeographicLocationService>();
+builder.Services.AddScoped<MaritalStatusService>();
+builder.Services.AddScoped<OfficialRecordService>();
+builder.Services.AddScoped<PhoneNumberTypeService>();
+builder.Services.AddScoped<SexService>();
+builder.Services.AddScoped<StatusService>();
+builder.Services.AddScoped<LookupService>();
+builder.Services.AddScoped<OfficerService>();
+builder.Services.AddScoped<EmailService>();
+builder.Services.AddScoped<ReportService>();
 
 var app = builder.Build();
 
@@ -23,7 +42,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ApplicationDbContext>();
     context.Database.Migrate(); // Apply pending migrations
-    GeographicLocationSeeder.SeedGeographicLocations(context);
+    DefaultSeeder.Run(context);
 }
 
 // Configure the HTTP request pipeline.
