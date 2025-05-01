786b72b40433658021962c678e07306a5a0f57a3
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-05-01 21:07:01 +0200

    add IApplicantService

---
+++ socialAssistanceFundMIS/Services/Applicants/IApplicantService.cs
@@ -0,0 +1,16 @@
+﻿using socialAssistanceFundMIS.Data;
+using SocialAssistanceFundMisMcv.ViewModels;
+
+namespace socialAssistanceFundMIS.Services.Applicants
+{
+    public interface IApplicantService
+    {
+        Task<ApplicantViewModel> CreateApplicantAsync(Applicant applicant, List<(string phoneNumber, int phoneNumberTypeId)> phoneNumbers);
+        Task<ApplicantViewModel> GetApplicantByIdAsync(int id);
+        Task<List<ApplicantViewModel>> GetAllApplicantsAsync();
+        Task<ApplicantViewModel> UpdateApplicantAsync(int id, Applicant applicant, List<string> phoneNumbers);
+        Task<bool> DeleteApplicantAsync(int id);
+        Task<bool> DeletePhoneNumberAsync(int applicantId, int phoneNumberId);
+    }
+
+}
