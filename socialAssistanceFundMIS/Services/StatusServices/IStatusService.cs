980b51d832adeecf79469f99e15448f6b33662e9
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:33:57 +0200

    add: status service

---
+++ socialAssistanceFundMIS/Services/StatusServices/IStatusService.cs
@@ -0,0 +1,14 @@
+﻿using socialAssistanceFundMIS.DTOs;
+
+namespace socialAssistanceFundMIS.Services.StatusServices
+{
+    public interface IStatusService
+    {
+        Task<StatusDTO> CreateStatusAsync(StatusDTO statusDTO);
+        Task<StatusDTO?> GetStatusByIdAsync(int id);
+        Task<List<StatusDTO>> GetAllStatusesAsync();
+        Task<StatusDTO> UpdateStatusAsync(int id, StatusDTO updatedStatusDTO);
+        Task DeleteStatusAsync(int id);
+        Task PermanentlyDeleteStatusAsync(int id);
+    }
+}
