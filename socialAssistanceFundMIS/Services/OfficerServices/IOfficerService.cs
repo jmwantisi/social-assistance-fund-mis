c56bcc34382f475b9faa604cf8af33c8382057bb
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:35:56 +0200

    readd: officer service

---
+++ socialAssistanceFundMIS/Services/OfficerServices/IOfficerService.cs
@@ -0,0 +1,13 @@
+﻿using socialAssistanceFundMIS.Models;
+
+namespace socialAssistanceFundMIS.Services.OfficerServices
+{
+    public interface IOfficerService
+    {
+        Task<IEnumerable<Officer>> GetAllAsync();
+        Task<Officer?> GetByIdAsync(int id);
+        Task<Officer> CreateAsync(Officer officer);
+        Task<Officer> UpdateAsync(int id, Officer officer);
+        Task<bool> DeleteAsync(int id);
+    }
+}
