82e217541b22fe17fd8a243fccb9e436bbc586a0
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:32:35 +0200

    refactor

--- socialAssistanceFundMIS/ViewModels/ApplicantViewModel.cs
+++ socialAssistanceFundMIS/ViewModels/ApplicantViewModel.cs
@@ -51,9 +51,9 @@ namespace socialAssistanceFundMIS.ViewModels
         // Get the appropriate day suffix (st, nd, rd, th)
         private string GetDaySuffix(int day)
         {
-            return (day % 10 == 1 && day != 11) ? "st" :
-                   (day % 10 == 2 && day != 12) ? "nd" :
-                   (day % 10 == 3 && day != 13) ? "rd" : "th";
+            return day % 10 == 1 && day != 11 ? "st" :
+                   day % 10 == 2 && day != 12 ? "nd" :
+                   day % 10 == 3 && day != 13 ? "rd" : "th";
         }
 
         public string? MaritalStatusName { get; set; }
