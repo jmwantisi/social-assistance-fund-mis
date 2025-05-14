117f6c136a7edabf10ebf6d6aa257ff1d434edb8
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:34:39 +0200

    readd: report service

---
+++ socialAssistanceFundMIS/Services/ReportServices/IReportService.cs
@@ -0,0 +1,10 @@
+﻿namespace socialAssistanceFundMIS.Services.ReportServices
+{
+    public interface IReportService
+    {
+        Task<(int Pending, int Approved)> GetApplicationStatusCountsAsync();
+        Task<(int Male, int Female)> GetApprovedApplicantsByGenderAsync();
+        Task<Dictionary<string, int>> GetApplicationCountsPerProgramAsync();
+
+    }
+}
