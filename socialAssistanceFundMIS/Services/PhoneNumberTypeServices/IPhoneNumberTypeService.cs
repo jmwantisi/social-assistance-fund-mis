e81c5beae4d100b7b4bbff79b494c5513822eaeb
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:34:57 +0200

    readd: phone type service

---
+++ socialAssistanceFundMIS/Services/PhoneNumberTypeServices/IPhoneNumberTypeService.cs
@@ -0,0 +1,12 @@
+﻿namespace socialAssistanceFundMIS.Services.PhoneNumberTypeServices
+{
+    public interface IPhoneNumberTypeService
+    {
+        Task<PhoneNumberTypeDTO> CreatePhoneNumberTypeAsync(PhoneNumberTypeDTO dto);
+        Task<PhoneNumberTypeDTO?> GetPhoneNumberTypeByIdAsync(int id);
+        Task<List<PhoneNumberTypeDTO>> GetAllPhoneNumberTypesAsync();
+        Task<PhoneNumberTypeDTO> UpdatePhoneNumberTypeAsync(int id, PhoneNumberTypeDTO dto);
+        Task<bool> DeletePhoneNumberTypeAsync(int id);
+        Task<bool> PermanentlyDeletePhoneNumberTypeAsync(int id);
+    }
+}
