1d18fb972719b0e976655e0f293dd337303d089a
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:36:15 +0200

    readd: marital service

---
+++ socialAssistanceFundMIS/Services/MaritalStatusServices/IMaritalStatusService.cs
@@ -0,0 +1,12 @@
+﻿namespace socialAssistanceFundMIS.Services.MaritalStatusServices
+{
+    public interface IMaritalStatusService
+    {
+        Task<MaritalStatusDTO> CreateMaritalStatusAsync(MaritalStatusDTO maritalStatusDto);
+        Task<MaritalStatusDTO?> GetMaritalStatusByIdAsync(int id);
+        Task<List<MaritalStatusDTO>> GetAllMaritalStatusesAsync();
+        Task<MaritalStatusDTO> UpdateMaritalStatusAsync(int id, MaritalStatusDTO updatedMaritalStatusDto);
+        Task<bool> DeleteMaritalStatusAsync(int id);
+        Task<bool> PermanentlyDeleteMaritalStatusAsync(int id);
+    }
+}
