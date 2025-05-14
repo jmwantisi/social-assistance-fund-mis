d477af81c6a179f68e890618acdb90f34df8d290
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:37:51 +0200

    readd: assistance service

---
+++ socialAssistanceFundMIS/Services/AssistanceProgramServices/IAssistanceProgramService.cs
@@ -0,0 +1,12 @@
+﻿namespace socialAssistanceFundMIS.Services.AssistanceProgramServices
+{
+    public interface IAssistanceProgramService
+    {
+        Task<AssistanceProgramDTO> CreateAssistanceProgramAsync(AssistanceProgramDTO dto);
+        Task<AssistanceProgramDTO?> GetAssistanceProgramByIdAsync(int id);
+        Task<List<AssistanceProgramDTO>> GetAllAssistanceProgramsAsync();
+        Task<AssistanceProgramDTO> UpdateAssistanceProgramAsync(int id, AssistanceProgramDTO dto);
+        Task<bool> DeleteAssistanceProgramAsync(int id);
+        Task<bool> PermanentlyDeleteAssistanceProgramAsync(int id);
+    }
+}
