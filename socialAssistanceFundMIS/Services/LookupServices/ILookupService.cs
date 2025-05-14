43cb6550443f4078119e5892087054749513b967
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:36:28 +0200

    readd: lookup service

---
+++ socialAssistanceFundMIS/Services/LookupServices/ILookupService.cs
@@ -0,0 +1,18 @@
+﻿using socialAssistanceFundMIS.Models;
+
+namespace socialAssistanceFundMIS.Services.LookupServices
+{
+    public interface ILookupService
+    {
+        Task<IEnumerable<AssistanceProgram>> GetProgramsAsync();
+        Task<AssistanceProgram?> GetProgramByIdAsync(int id);
+        Task<IEnumerable<Sex>> GetSexesAsync();
+        Task<Sex?> GetSexByIdAsync(int id);
+        Task<IEnumerable<MaritalStatus>> GetMaritalStatusesAsync();
+        Task<MaritalStatus?> GetMaritalStatusByIdAsync(int id);
+        Task<IEnumerable<Status>> GetStatusesAsync();
+        Task<Status?> GetStatusByIdAsync(int id);
+        Task<IEnumerable<PhoneNumberType>> GetPhoneNumberTypesAsync();
+        Task<PhoneNumberType?> GetPhoneNumberTypeByIdAsync(int id);
+    }
+}
