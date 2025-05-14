f313dd89ec347b27f7baa6ed0601c3b487c629a0
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:37:28 +0200

    readd: designation service

---
+++ socialAssistanceFundMIS/Services/DesignationServices/IDesignationService.cs
@@ -0,0 +1,12 @@
+﻿namespace socialAssistanceFundMIS.Services.DesignationServices
+{
+    public interface IDesignationService
+    {
+        Task<DesignationDTO> CreateDesignationAsync(DesignationDTO designationDto);
+        Task<DesignationDTO?> GetDesignationByIdAsync(int id);
+        Task<List<DesignationDTO>> GetAllDesignationsAsync();
+        Task<DesignationDTO> UpdateDesignationAsync(int id, DesignationDTO updatedDesignationDto);
+        Task<bool> DeleteDesignationAsync(int id);
+        Task<bool> PermanentlyDeleteDesignationAsync(int id);
+    }
+}
