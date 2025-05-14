a1b7c0449b001e05e088ab33cf4b0ce0fffb34b3
James Mwantisi <jmwantisi@gmail.com>
2025-05-14 14:34:25 +0200

    readd: gender service

---
+++ socialAssistanceFundMIS/Services/SexServices/ISexService.cs
@@ -0,0 +1,12 @@
+﻿namespace socialAssistanceFundMIS.Services.SexServices
+{
+    public interface ISexService
+    {
+        Task<SexDTO> CreateSexAsync(SexDTO sexDto);
+        Task<SexDTO?> GetSexByIdAsync(int id);
+        Task<List<SexDTO>> GetAllSexesAsync();
+        Task<SexDTO> UpdateSexAsync(int id, SexDTO updatedSexDto);
+        Task<bool> DeleteSexAsync(int id);
+        Task<bool> PermanentlyDeleteSexAsync(int id);
+    }
+}
