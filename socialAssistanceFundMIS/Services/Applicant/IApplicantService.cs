0f4c8862cf2a33a260275672ec54968dc8d972d5
James Mwantisi <jmwantisi@gmail.com>
2025-09-03 20:03:17 +0200

    refactor service: Application Service

--- socialAssistanceFundMIS/Services/Applicant/IApplicantService.cs
+++ socialAssistanceFundMIS/Services/Applicant/IApplicantService.cs
@@ -1,16 +1,19 @@
-﻿using socialAssistanceFundMIS.Data;
+﻿using socialAssistanceFundMIS.Common;
+using socialAssistanceFundMIS.Data;
 using socialAssistanceFundMIS.ViewModels;
 
 namespace socialAssistanceFundMIS.Services.Applicants
 {
     public interface IApplicantService
     {
-        Task<ApplicantViewModel> CreateApplicantAsync(Applicant applicant, List<(string phoneNumber, int phoneNumberTypeId)> phoneNumbers);
-        Task<ApplicantViewModel> GetApplicantByIdAsync(int id);
-        Task<List<ApplicantViewModel>> GetAllApplicantsAsync();
-        Task<ApplicantViewModel> UpdateApplicantAsync(int id, Applicant updatedApplicant, List<(string phoneNumber, int phoneNumberTypeId)> phoneNumbers);
-        Task<bool> DeleteApplicantAsync(int id);
-        Task<bool> DeletePhoneNumberAsync(int applicantId, int phoneNumberId);
+        Task<Result<ApplicantViewModel>> CreateApplicantAsync(Applicant applicant, List<(string phoneNumber, int phoneNumberTypeId)> phoneNumbers);
+        Task<Result<ApplicantViewModel>> GetApplicantByIdAsync(int id);
+        Task<Result<List<ApplicantViewModel>>> GetAllApplicantsAsync();
+        Task<Result<ApplicantViewModel>> UpdateApplicantAsync(int id, Applicant updatedApplicant, List<(string phoneNumber, int phoneNumberTypeId)> phoneNumbers);
+        Task<Result> DeleteApplicantAsync(int id);
+        Task<Result> DeletePhoneNumberAsync(int applicantId, int phoneNumberId);
+        Task<Result<List<ApplicantViewModel>>> SearchApplicantsAsync(string searchTerm, int page = 1, int pageSize = 20);
+        Task<Result<bool>> IsIdentityCardNumberUniqueAsync(string identityCardNumber, int? excludeId = null);
+        Task<Result<bool>> IsEmailUniqueAsync(string email, int? excludeId = null);
     }
-
 }
